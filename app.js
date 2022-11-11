51406e23fca4a09534c415b8b5491043dd881ea9
vehicleLoan <vehicleloan920@gmail.com>
2022-11-12 00:01:23 +0530

    add gitignore, procfile

--- app.js
+++ app.js
@@ -178,6 +178,6 @@ app.post("/search",function(req,res)
 
 
 
+let port=process.env.PORT||3000;
 
-
-app.listen(3000,function(){console.log("server running on port 3000")})
+app.listen(port,function(){console.log("server running on port 3000")})
